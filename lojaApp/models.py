5b886071b74b0ae207f2a06cf2035b2cf4f7daf6
Gustavo <go609127@gmail.com>
2022-11-02 17:45:59 -0300

    base de dados

--- lojaApp/models.py
+++ lojaApp/models.py
@@ -1,3 +1,12 @@
+from tokenize import blank_re
 from django.db import models
 
-# Create your models here.
+class Produtos(models.Model):
+    titulo = models.CharField(max_length=100)
+    preco = models.CharField(max_length=10)
+    tamanhos = models.CharField(max_length=5)
+    image = models.CharField(max_length=255)
+    desc = models.CharField(max_length=1000,blank=True)
+
+    def __str__(self):
+        return self.titulo
