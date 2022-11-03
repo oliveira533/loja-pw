fb2ad98fef5d5e3fc0cd8ebef832fd87373d293d
Gustavo <go609127@gmail.com>
2022-11-02 21:17:05 -0300

    socorro

--- lojaApp/views.py
+++ lojaApp/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from .models import Produtos
 
 # Create your views here.
 
 def index(request):
-    return render(request, 'lojaSite/index.html')
+    produtos = Produtos.objects.all
+    return render(request, 'lojaSite/index.html',{'produtos' : produtos})
--- lojaSite/settings.py
+++ lojaSite/settings.py
@@ -119,7 +119,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'templates/static')
+    os.path.join(BASE_DIR, 'templates/static'),
 ]
 
 # Default primary key field type
